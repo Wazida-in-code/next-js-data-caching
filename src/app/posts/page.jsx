cb0e5180a9ca6dbe656c01be4a45135425e707d9
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-17 21:56:58 +0600

    Data-fetching-new-method-1

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const PostsPage = () => {
+const getPost = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return res.json()
+}
+
+const PostsPage = async() => {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const posts = await res.json();
+
+    const posts = await getPost();
+    console.log(posts);
+
     return (
         <div>
             <h2>Hello Posts</h2>
+            <h3>Posts: {posts.length}</h3>
         </div>
     );
 };
