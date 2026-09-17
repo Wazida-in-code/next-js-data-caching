4d08993df70940a2f91756e911a597dc24e29e7e
Wazida-in-code <wazidamomtazesha@gmail.com>
2026-09-17 22:08:33 +0600

    Data-fetching-different-method

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
+//option-2-data-fetching
 const getPost = async() => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     return res.json()
 }
 
+//option-1-data-fetching
 const PostsPage = async() => {
     // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     // const posts = await res.json();
+
+//option-3-data-fetching
+const getPost2 = async() => {
+    try{
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        return res.json()
+    }
+    catch(error){
+        throw new Error("Failed to load data!")
+    }
+}
+
+//option-4-data-fetching
+const getPost = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    
+    if (!res.ok){
+        throw new Error("Failed to load data!")
+    }
+}
+
 
     const posts = await getPost();
     console.log(posts);
